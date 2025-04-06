932a9d3084df7230f15db1ef64e70ad7951eb3d0
yfitjapan <fujiiyousay@gmail.com>
2025-04-06 13:34:18 +0900

    0406debug

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -360,7 +360,15 @@ export default function SubscriptionPage() {
             className="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col"
           >
             <div className="flex-1">
+            <div className="flex items-center">
+              <div className="w-8 h-8 mr-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="text-gray-600">
+                  <path fillRule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625zM7.5 15a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 017.5 15zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H8.25z" clipRule="evenodd" />
+                  <path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
+                </svg>
+              </div>
               <h3 className="text-xl font-semibold text-gray-900">無料プラン</h3>
+            </div>
               <p className="mt-4 flex items-baseline text-gray-900">
                 <span className="text-5xl font-extrabold tracking-tight">¥0</span>
                 <span className="ml-1 text-xl font-semibold">/月</span>
