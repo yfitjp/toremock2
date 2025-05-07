3ca882680976e42fbf34db12ff531e4ea962459a
yfitjapan <fujiiyousay@gmail.com>
2025-05-08 06:42:36 +0900

    0508debug

--- src/app/exams/[id]/take/ExamForm.tsx
+++ src/app/exams/[id]/take/ExamForm.tsx
@@ -36,7 +36,7 @@ export default function ExamForm({
   const formatTime = (totalSeconds: number) => {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
-    return `${minutes.toString().padStart(2, '0')}分${seconds.toString().padStart(2, '0')}秒`;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   };
 
   const handleAnswerSelect = (questionId: string, optionIndex: number) => {
